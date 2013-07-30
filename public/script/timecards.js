4933fae0401f47916a3a4ae040b0f7338278da73
Kevin Hickey <khickey@thoughtworks.com>
2013-07-30 14:52:16 -0500

    Added Monthly Report for July only (for now)

--- public/script/timecards.js
+++ public/script/timecards.js
@@ -4,6 +4,8 @@ angular.module('Timecards', []).
             when('/timecards', {templateUrl: 'partials/timecard_summary.html',   controller: TimecardSummaryController}).
             when('/enter_time', {templateUrl: 'partials/timecard_enter_time.html', controller: TimecardController}).
             when('/detail/:beeline_guid', {templateUrl: 'partials/timecard_detail.html', controller: TimecardDetailController}).
+            when('/monthly_report/:month/:year', {templateUrl: 'partials/timecard_monthly_report.html', controller: TimecardMonthlyReportController}).
+            when('/monthly_report', {redirectTo: '/monthly_report/07/2013'}).
             otherwise({redirectTo: '/timecards'});
     }]);
 
@@ -121,5 +123,91 @@ function TimecardDetailController($scope, $http, $routeParams) {
     $http.get("rates").success(function(rates) {
        $scope.rates = rates;
     });
+
+}
+
+function TimecardMonthlyReportController($scope, $http, $routeParams, $filter) {
+    console.log($routeParams);
+    $scope.report_month = parseInt($routeParams.month);
+    $scope.report_year = parseInt($routeParams.year);
+
+    $http.get("consultants/list").success(function(consultants) {
+        $scope.consultants = consultants;
+        $http.get("rates").success(function(rates) {
+            $scope.rates = rates;
+            $http.get("timecard/list_existing").success(function(existing_ending_dates) {
+                $scope.existing_end_dates = existing_ending_dates;
+                $scope.end_dates_in_month = endDatesInMonth(existing_ending_dates, $scope.report_month, $scope.report_year);
+
+                buildReportTable(consultants, rates, $scope.end_dates_in_month);
+                calculateTotals(consultants);
+            });
+        });
+    });
+
+    function calculateTotals(consultants) {
+        $scope.total_fees = 0;
+        $scope.total_fees_discounted = 0;
+
+        consultants.forEach(function(consultant) {
+           $scope.total_fees += consultant.total_fees;
+           $scope.total_fees_discounted += consultant.total_fees_discount;
+        });
+
+        $scope.expenses = $scope.total_fees * 0.12;
+    }
+
+    function endDatesInMonth(existing_end_dates, month_string, year_string) {
+        var month = parseInt(month_string);
+        var year = parseInt(year_string);
+        var end_dates_in_month = existing_end_dates.filter(function(end_date) {
+            var date = new Date(end_date);
+            return (((date.getMonth() + 1) == month) && (date.getFullYear() == year))
+        });
+
+        return end_dates_in_month;
+    }
+
+    function buildReportTable(consultants, rates, end_dates_in_month) {
+        buildTableHeaders(end_dates_in_month);
+        buildTableLines(consultants, end_dates_in_month);
+    };
+
+    function buildTableHeaders(end_dates) {
+        $scope.table_headers = [];
+        $scope.table_headers.push("Name");
+        end_dates.forEach(function(end_date) {
+            $scope.table_headers.push("Hours Worked " + end_date);
+            $scope.table_headers.push("Hours Submitted " + end_date);
+        });
+        $scope.table_headers.push("Total Hours Submitted");
+        $scope.table_headers.push("Fees");
+        $scope.table_headers.push("Fees With Discount");
+    };
+
+    function buildTableLines(consultants, end_dates) {
+        $scope.table_lines = [];
+        consultants.forEach(function(consultant) {
+            consultant.hours_submitted_month = 0;
+            var line = [];
+            line.push(consultant.first_name + " " + consultant.last_name);
+            end_dates.forEach(function(end_date) {
+                consultant.timecards.forEach(function(timecard) {
+                    if (timecard.week_ending == end_date) {
+                        line.push(timecard.hours_worked);
+                        line.push(timecard.hours_submitted);
+                        consultant.hours_submitted_month += timecard.hours_submitted;
+                    }
+                })
+            });
+            line.push(consultant.hours_submitted_month);
+            consultant.total_fees = consultant.hours_submitted_month * $scope.rates[consultant.grade];
+            line.push($filter('currency')(consultant.total_fees, "$"));
+            consultant.total_fees_discount = consultant.total_fees * .91;
+            line.push($filter('currency')(consultant.total_fees_discount, "$"));
+            $scope.table_lines.push(line);
+
+        });
+    }
 
 }
